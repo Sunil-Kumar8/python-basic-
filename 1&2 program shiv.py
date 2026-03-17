a14279a5072f628a2fb1629951124cf521f54322
sunil kumar <shivroy9123@gmail.com>
2026-03-17 15:24:30 +0530

    Add files via upload

---
+++ 1&2 program shiv.py
@@ -0,0 +1,30 @@
+#1st day
+print("its sunil this side","tech lead in google")
+print(32)
+print (35*12)
+
+#2nd day
+name = "sunil kumar" 
+age = 21
+money = 4.1 
+
+print(age)
+print(money)
+print(name)
+
+print("my name is :", name)
+print("my age :", age)
+print("amount of money i have :", money)
+
+print(type(name) )
+print(type(age) )
+print(type(money) )
+
+
+
+age = 23
+old = True
+a = None
+print(type(age))
+print(type(a))
+print(type(old))
---
+++ 3&4 program shiv.py
@@ -0,0 +1,15 @@
+#3rd day
+a = 200
+b =808
+sum = a + b 
+print(sum)
+diff = a-b
+print(diff)
+
+
+#4th day
+#taking input from user &print it
+name = input("name :")
+age = int(input("age :"))
+money = float(input("money :"))
+print("my name is", name,"my age is", age,"year", "and i have save", money,"this year")
